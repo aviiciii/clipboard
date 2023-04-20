46ed9b83b7829d74d962d1065c0dce080810e093
aviiciii <laavesh1@gmail.com>
2023-04-20 19:05:06 +0530

    creating firebase realtime database api

--- api.py
+++ api.py
@@ -1,20 +1,54 @@
-# flask app to store and retrieve data from the database
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+import json
 
 from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///clipboard.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
 
-class Data(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.String(100), nullable=False)
+@app.route('/', methods=['GET'])
+def home():
+    return "Hello World"
 
-    def __repr__(self):
-        return f"{self.id} - {self.data}"
-    
+cred = credentials.Certificate("serviceaccountkey.json")
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# Initialize the app with a service account, granting admin privileges
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://clipboard-c6b51-default-rtdb.firebaseio.com/'
+})
+
+
+@app.route('/api', methods=['GET'])
+def api():
+    if request.method == 'GET':
+        
+        ref = db.reference('/')
+
+        # read ref as json data
+        snapshot = ref.order_by_key().get()
+
+        # reverse the ordered dict by key to get the latest data
+        snapshot = dict(reversed(list(snapshot.items())))
+
+        print(snapshot)
+
+        return jsonify(snapshot)
+
+
+@app.route('/api', methods=['POST'])
+def api_post():
+    if request.method == 'POST':
+        # check if the post request has the json data
+        if not request.is_json:
+            return jsonify({"msg": "Missing JSON in request"}), 400
+        
+        # get the json data
+
+        data = request.get_json()
+        
+        print(data)
+
+        ref = db.reference('/')
+        ref.push(data)
+        return jsonify(data)
