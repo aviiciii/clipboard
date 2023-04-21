753c4cc8da1b05caa4a1eee57fd07d5daa86ff2e
aviiciii <laavesh1@gmail.com>
2023-04-21 19:34:32 +0530

    able to save data in firebase realtime database

--- api.py
+++ api.py
@@ -18,6 +18,8 @@ firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://clipboard-c6b51-default-rtdb.firebaseio.com/'
 })
 
+def generate_key(latest_value):
+    return int(latest_value) + 1 if latest_value else 1
 
 @app.route('/api', methods=['GET'])
 def api():
@@ -44,11 +46,36 @@ def api_post():
             return jsonify({"msg": "Missing JSON in request"}), 400
         
         # get the json data
-
         data = request.get_json()
-        
-        print(data)
 
+        # get the clipboard data
+        clip = data['body']
+        print(clip)
+
+        # get the database reference
         ref = db.reference('/')
-        ref.push(data)
+
+        # read ref as json data
+        snapshot = ref.order_by_key().get()
+
+        # reverse the ordered dict by key to get the latest data
+        snapshot = dict(reversed(list(snapshot.items())))
+        latest_key = list(snapshot.keys())[0]
+        print(latest_key)
+
+        # get new key
+        new_key = generate_key(latest_key)
+        print(new_key)
+
+        # new clipboard data
+        new_clip = {'data': clip}
+        new_clip = json.dumps(new_clip)
+        # get the database reference
+        ref = db.reference('/')
+        
+        # update the database
+        ref.child(str(new_key)).set({
+            'data': clip
+        })
+
         return jsonify(data)
